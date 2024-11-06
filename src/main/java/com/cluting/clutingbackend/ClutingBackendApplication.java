03b9bfbac7c636676a4e8286fa735659489acd27
lej8924 <eunjae8924@naver.com>
2024-11-06 16:03:53 +0900

    first commit

---
+++ src/main/java/com/cluting/clutingbackend/ClutingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cluting.clutingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClutingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClutingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cluting/clutingbackend/ClutingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluting.clutingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClutingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
