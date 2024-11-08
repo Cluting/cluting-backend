5922121e0494ae09c17191ea338c4f6055483cc2
lej8924 <eunjae8924@naver.com>
2024-11-08 15:46:29 +0900

    ➕ Add: swagger 의존성 추가

--- src/main/java/com/cluting/clutingbackend/ClutingBackendApplication.java
+++ src/main/java/com/cluting/clutingbackend/ClutingBackendApplication.java
@@ -2,8 +2,9 @@ package com.cluting.clutingbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ClutingBackendApplication {
 
     public static void main(String[] args) {
