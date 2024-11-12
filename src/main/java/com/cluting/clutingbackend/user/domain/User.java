d5032c79a06aeeb0f0738196090f096185bd19a9
zestbear <zestbear90@gmail.com>
2024-11-12 13:53:13 +0900

    Feat: 사용자 도메인 추가

---
+++ src/main/java/com/cluting/clutingbackend/user/domain/User.java
@@ -0,0 +1,32 @@
+package com.cluting.clutingbackend.user.domain;
+
+import com.cluting.clutingbackend.user.domain.enums.Role;
+import com.cluting.clutingbackend.user.domain.enums.Semester;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "tb_user")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private String phone;
+    private String location;
+    private String school;
+    private Boolean isOnLeaver;
+    @Enumerated(EnumType.STRING)
+    private Semester semester;
+    private String major;
+    private String doubleMajor;
+    private String profile;
+}
---
+++ src/main/java/com/cluting/clutingbackend/user/domain/enums/Role.java
@@ -0,0 +1,5 @@
+package com.cluting.clutingbackend.user.domain.enums;
+
+public enum Role {
+    GENERAL, ADMIN
+}
