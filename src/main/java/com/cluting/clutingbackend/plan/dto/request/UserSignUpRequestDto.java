4abbfd41e3d02981a4856e5f57fc4d99d508f9ff
zestbear <zestbear90@gmail.com>
2024-11-16 14:30:03 +0900

    Fix: Enum 파일 제거

--- src/main/java/com/cluting/clutingbackend/plan/dto/request/UserSignUpRequestDto.java
+++ src/main/java/com/cluting/clutingbackend/plan/dto/request/UserSignUpRequestDto.java
@@ -1,7 +1,5 @@
 package com.cluting.clutingbackend.plan.dto.request;
 
-import com.cluting.clutingbackend.plan.domain.enums.Role;
-import com.cluting.clutingbackend.plan.domain.enums.Semester;
 import com.cluting.clutingbackend.plan.domain.User;
 import lombok.Getter;
 
@@ -14,14 +12,14 @@ public class UserSignUpRequestDto {
     private String location;
     private String school;
     private Boolean isOnLeaver;
-    private Semester semester;
+    private User.Semester semester;
     private String major;
     private String doubleMajor;
 
     public User toEntity(String encoded) {
         return User.builder()
                 .name(name)
-                .role(Role.GENERAL)
+                .role(User.Role.USER)
                 .email(email)
                 .password(encoded)
                 .phone(phone)
