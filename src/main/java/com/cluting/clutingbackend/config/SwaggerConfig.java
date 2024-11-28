0a072c9bd23b712304d0ca8c7634cbe3b507e6c5
lej8924 <eunjae8924@naver.com>
2024-11-28 15:24:25 +0900

    ➕ Add: config 수정

---
+++ src/main/java/com/cluting/clutingbackend/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.cluting.clutingbackend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Cluting Swagger API명세서", version = "v1"))
+public class SwaggerConfig {
+
+    // Bearer 토큰을 사용할 수 있도록 설정
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(Type.HTTP)
+                                .scheme("bearer")
+                                .in(In.HEADER)
+                                .bearerFormat("JWT")
+                                .name("Authorization"))
+                );
+    }
+}
