e52cb0c1542b721dd3d95ca40dbf615d0c54978b
lej8924 <eunjae8924@naver.com>
2024-11-28 15:47:59 +0900

    🐛 Fix: UserSignInDto 어노테이션 수정

--- src/main/java/com/cluting/clutingbackend/plan/dto/request/UserSignInRequestDto.java
+++ src/main/java/com/cluting/clutingbackend/plan/dto/request/UserSignInRequestDto.java
@@ -1,8 +1,12 @@
 package com.cluting.clutingbackend.plan.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserSignInRequestDto {
     private String email;
     private String password;
