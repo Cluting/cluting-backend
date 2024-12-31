a3b22a8be32d7f9877328359c036975c0ce974a9
lej8924 <eunjae8924@naver.com>
2024-12-31 09:02:17 +0900

    ➕ Add: currentstage api 추가

--- src/main/java/com/cluting/clutingbackend/global/enums/CurrentStage.java
+++ src/main/java/com/cluting/clutingbackend/global/enums/CurrentStage.java
@@ -12,6 +12,25 @@ public enum CurrentStage {
     DOC_PASS("서류합격자 및 면접안내"),
     EVAL("면접 평가하기"),
     FINAL_PASS("최종합격자 및 활동안내");
-
     private final String description;
+    private StageState currentState; // 현재 상태를 저장하는 필드
+    // 초기 상태 설정 (진행 전)
+    static {
+        for (CurrentStage stage : CurrentStage.values()) {
+            stage.currentState = StageState.BEFORE;
+        }
+    }
+    // 상태 변경 메서드
+    public void updateState(StageState newState) {
+        this.currentState = newState;
+    }
+    @Getter
+    @RequiredArgsConstructor
+    public enum StageState {
+        BEFORE("진행 전"),
+        IN_PROGRESS("진행 중"),
+        AFTER("진행 후");
+
+        private final String stateDescription;
+    }
 }
