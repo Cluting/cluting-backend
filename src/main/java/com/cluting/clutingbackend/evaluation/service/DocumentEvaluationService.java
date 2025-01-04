6be6e668fb9ac8d006b569ba3ff0503e7573b962
Suanna01 <yangnony01@gmail.com>
2025-01-04 19:31:23 +0900

    Fix : 서류평가 불러오기 수정

--- src/main/java/com/cluting/clutingbackend/evaluation/service/DocumentEvaluationService.java
+++ src/main/java/com/cluting/clutingbackend/evaluation/service/DocumentEvaluationService.java
@@ -503,6 +503,7 @@ public class DocumentEvaluationService {
                 .orElseThrow(() -> new ResourceNotFoundException("Club User not found"));
         EvaluatorScores myEvaluation = EvaluatorScores.ofForUser(evaluators.get(0), currentClubUser, documentEvalScoreRepository);
 
+
         return new DocumentEvaluation4Response(
                 applicantInfo,
                 questionAndAnswers,
