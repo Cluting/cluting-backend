2869b4a6e2ea4171b2ebb4b151bc9f2fc71c20f1
lej8924 <eunjae8924@naver.com>
2025-01-07 11:40:21 +0900

    🐛 Fix: option 생성자 설정 수정

--- src/main/java/com/cluting/clutingbackend/plan/domain/Option.java
+++ src/main/java/com/cluting/clutingbackend/plan/domain/Option.java
@@ -1,15 +1,15 @@
 package com.cluting.clutingbackend.plan.domain;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
+import lombok.*;
 
 @Entity
 @Data
 @Getter
 @Table(name ="tb_option")
 @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Option {
 
     @Id
@@ -25,6 +25,7 @@ public class Option {
 
     @Column
     private boolean isCorrect; // 정답인지/아닌지?
+
 
 }
 
