a1dba8e24a3394bda5bcee84c04e20beba906890
lej8924 <eunjae8924@naver.com>
2025-01-08 10:50:46 +0900

    🐛 Fix: ClubUserPermission table 생성

--- src/main/java/com/cluting/clutingbackend/clubuser/domain/ClubUserPermission.java
+++ src/main/java/com/cluting/clutingbackend/clubuser/domain/ClubUserPermission.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Table(name = "tb_club_user")
+@Table(name = "tb_club_user_permission")
 public class ClubUserPermission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
