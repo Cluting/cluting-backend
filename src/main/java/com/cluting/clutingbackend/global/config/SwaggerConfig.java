6d90546da7ddc7505dc5334786ec1b259567f936
lej8924 <eunjae8924@naver.com>
2025-01-13 16:15:15 +0900

    🐛 Fix: swagger에 nginx 리버스 프록시 설정

--- src/main/java/com/cluting/clutingbackend/global/config/SwaggerConfig.java
+++ src/main/java/com/cluting/clutingbackend/global/config/SwaggerConfig.java
@@ -3,25 +3,26 @@ package com.cluting.clutingbackend.global.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Cluting Swagger API명세서", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Cluting Swagger API 명세서", version = "v1"))
 public class SwaggerConfig {
 
-//    @Bean
-//    public SwaggerUiConfigProperties swaggerUiConfigProperties() {
-//        SwaggerUiConfigProperties configProperties = new SwaggerUiConfigProperties();
-//        configProperties.setFilter("PATCH"); // Filter 활성화
-//        return configProperties;
-//    }
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server()
+                                .url("https://210.107.205.122:20025") // NGINX URL
+                                .description("Production Server")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
